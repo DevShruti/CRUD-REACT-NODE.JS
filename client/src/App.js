7b6e60bf016502425c6ba88ee3de282a3199b569
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-09-13 09:49:17 -0500

    inputs

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+    
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+  
+        />
+        <button>Add Employee</button>
+      </div>
     </div>
   );
 }
